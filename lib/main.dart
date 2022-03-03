ab1c6e44242310a371aaacb1a21cff3385ed2381
emirhalici <emir.halici1@gmail.com>
2022-03-03 11:02:25 +0300

    add new word manual screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:english_dictionary/components/wordCard.dart';
+import 'package:english_dictionary/screens/add_word_manually_page.dart';
 import 'package:english_dictionary/themes.dart';
 import 'package:flutter/material.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -17,97 +16,7 @@ class MyApp extends StatelessWidget {
       theme: lightTheme,
       darkTheme: darkTheme,
       themeMode: ThemeMode.system,
-      home: const MyHomePage(title: 'English Dictionary'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.headline2,
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-              Text(
-                'Good afternoon, Emir',
-                style: Theme.of(context).textTheme.headline1,
-              ),
-              Text(
-                'Exchange Rates',
-                style: Theme.of(context).textTheme.headline2,
-              ),
-              Text(
-                'headline 3',
-                style: Theme.of(context).textTheme.headline3,
-              ),
-              Text(
-                'bodytext 1',
-                style: Theme.of(context).textTheme.bodyText1,
-              ),
-              Text(
-                'bodytext 2',
-                style: Theme.of(context).textTheme.bodyText2,
-              ),
-              TextButton(
-                style: button1(Theme.of(context)),
-                onPressed: () {},
-                child: const Text('button1'),
-              ),
-              ElevatedButton(
-                style: button2(Theme.of(context)),
-                onPressed: () {},
-                child: const Text('Search Word'),
-              ),
-              OutlinedButton(
-                style: button3(Theme.of(context)),
-                onPressed: () {},
-                child: const Text('Add Manually'),
-              ),
-              WordCard(
-                type: 'noun',
-                definition: 'Exemption from punishment or freedom from the injurious consequences of an action.',
-              ),
-            ],
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: AddWordManuallyPage(title: 'English Dictionary'),
     );
   }
 }
---
+++ lib/screens/add_word_manually_page.dart
@@ -0,0 +1,89 @@
+import 'package:english_dictionary/themes.dart';
+import 'package:flutter/material.dart';
+
+class AddWordManuallyPage extends StatelessWidget {
+  final TextEditingController controllerWord = TextEditingController();
+  final TextEditingController controllerType = TextEditingController();
+  final TextEditingController controllerDefinition = TextEditingController();
+  final TextEditingController controllerExample = TextEditingController();
+  final String title;
+  AddWordManuallyPage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'English Dictionary',
+        ),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const SizedBox(height: 16),
+            Text(
+              'Add Word',
+              style: Theme.of(context).textTheme.headline2?.copyWith(fontSize: 36),
+              textAlign: TextAlign.start,
+            ),
+            const SizedBox(height: 26),
+            TextField(
+              controller: controllerWord,
+              decoration: textFieldDecorationWithHint(context, 'Enter word', 'i.e apple'),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: controllerType,
+              decoration: textFieldDecoration(context, 'Enter word type'),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: controllerDefinition,
+              minLines: 2,
+              maxLines: 3,
+              textAlignVertical: TextAlignVertical.center,
+              decoration: textFieldDecoration(context, 'Enter definition'),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: controllerExample,
+              minLines: 1,
+              maxLines: 3,
+              decoration: textFieldDecoration(context, 'Enter an example sentence'),
+            ),
+            const SizedBox(height: 16),
+            IntrinsicHeight(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      style: button2(context, Theme.of(context), 8),
+                      onPressed: () {},
+                      child: const Text('Add Word', style: TextStyle(fontSize: 18)),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                    child: TextButton(
+                      style: button1(context, Theme.of(context), 8),
+                      onPressed: () {},
+                      child: const Text('Cancel', style: TextStyle(fontSize: 18)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
